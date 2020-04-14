20fdca136c48fc0924fa64a1f616c9fc48617d18
nicoleluguesi@gmail.com <nicoleluguesi.student@careerdevs.com>
2020-04-14 16:12:13 -0400

    added css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React, {Component } from 'react';
 import './App.css';
+import Post from './components/Post';
+import posts from './mock/posts';
 
 class App extends Component {
+  renderPosts = () => {
+      const display = posts.map(post => {
+        return <Post post={post} />;
+      });
+      return display;
+    }
 
   render() {
     return (
+
+      
       <div className="App">
-        <h1>Welcome to BrainHive</h1>
+        <div className="card">
+        
+        </div>
+        <h1>Welcome to Brain Hive</h1>
+        {/* <Post post={this.posts[0]}/> */}
+        <div className="postList">
+        {this.renderPosts()}
+      </div>
       </div>
     );
    }
---
+++ src/components/Post.js
@@ -0,0 +1,23 @@
+  
+import React from 'react';
+
+const Post = ({post}) => {
+
+  // the paramater is doing this:
+  // const {post} = props;
+
+  return (
+    <div className={'box'}>
+    
+
+      <h2 id="title">{post.title}</h2>
+      <p>{post.resourceAuthor}</p>
+      {post.videoLength ? (<p>Length: {post.videoLength}</p>) : null }
+      {/* {conditional ? true code : false code} */}
+      <p>Rating: {post.rating}</p>
+      <p>Comments: {post.comments.length}</p>
+    </div>
+  )
+}
+
+export default Post;
